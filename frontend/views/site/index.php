7c2da3cf4e65a8ce4e062479feb8b5f35b5a9c63
lemayi <xiaoxiaolemayi@gmail.com>
2014-12-04 17:57:12 +0800

    update the home page

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -14,7 +14,7 @@
 <body>
 <div class="wrap">
     <!-- header start -->
-    <div class="header_wrap">
+    <div class="header-wrap">
         <div class="header cc">
             <!-- logo start -->
             <div class="logo">
@@ -39,7 +39,7 @@
             </div>
             <!-- nav end -->
             <!-- header right start -->
-            <div class="header_right">
+            <div class="header-right">
                 <a href="#" rel="nofollow">登录</a>
                 <a href="#" rel="nofollow">注册</a>
             </div>
@@ -47,22 +47,174 @@
         </div>
     </div>
     <!-- header end -->
-    <div class="main_wrap">
+    <!-- center start -->
+    <div class="main-wrap">
         <div class="main cc">
-            <div class="main_left">
-                <div class="main_left_nav">
+            <!-- center left start -->
+            <div class="main-left">
+                <!-- center left top start -->
+                <div class="left-top cc">
                     <ul>
-                        <li class="current"><a href="#">最热</a></li>
+                        <li class="current"><a href="#">24小时热门</a></li>
+                        <li><a href="#">一周热门</a></li>
                         <li><a href="#">最新</a></li>
                     </ul>
-                    <div class="content_filter"></div>
+                    <a class="btn-publish">发布</a>
                 </div>
-                <div class="main_left_box"></div>
+                <!-- center left top end -->
+                <!-- center left box start -->
+                <div class="left-box">
+                    <div class="item cc">
+                        <div class="pic">
+                            <a href="#"><img src="http://cdn.v2ex.com/avatar/e923/7191/77126_normal.png?m=1413276027" alt=""></a>
+                        </div>
+                        <div class="content cc">
+                            <div class="part1">
+                                <a href="#">【新闻解读！】老同志两个月前去国家博物馆，昨天新闻才爆出。有四个细节值得注意：1/老同志精神矍铄！2/老同志深切回忆1997年香港回归祖国盛况！3/老同志又双叒叕题词了！4/老同志最后去看的画展，作者许江，现任中国美术学院院长，他妈叫江泽南！ </a>
+                            </div>
+                            <div class="part2">
+                                腾讯科技讯据知情人士周四透露，三星电子移动部门的三位副职主管可能会在近期离职，原因是三星电子的利润将大幅下滑。由于涉及的事务非常敏感，因此这位知情人士不愿透露自己的身份。据他讲，这三位副职都是直接向部门主管申宗均（J.K.Shin）报...
+                            </div>
+                            <div class="part3">
+                                <a class="digg-up">顶 100</a>
+                                <a class="digg-down">踩 100</a>
+                                <a class="reply">评论 123</a>
+                                <a class="like">收藏</a>
+                                <span class="tags">
+                                    <a href="#">曼联</a>
+                                    <a href="#">范加尔</a>
+                                    <a href="#">格泽尔家族</a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item cc">
+                        <div class="pic">
+                            <a href="#"><img src="http://cdn.v2ex.com/avatar/e45c/2dc8/13106_normal.png?m=1334956121" alt=""></a>
+                        </div>
+                        <div class="content cc">
+                            <div class="part1">
+                                <a href="#">次世代党党首望石原慎太郎落选也担任最高顾问 </a>
+                            </div>
+                            <div class="part2">
+                                【共同社12月4日电】日本次世代党党首平沼赳夫3日在日本外国记者协会举行记者会，就众院选举比例代表东京区中该党名单中最后一位、即第9位候选人石原慎太郎（82岁）表示“希望他即使不获得席位也能担任最
+                            </div>
+                            <div class="part3">
+                                <a class="digg-up">顶 100</a>
+                                <a class="digg-down">踩 100</a>
+                                <a class="reply">评论 123</a>
+                                <a class="like">收藏</a>
+                                <span class="tags">
+                                    <a href="#">曼联</a>
+                                    <a href="#">范加尔</a>
+                                    <a href="#">格泽尔家族</a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item cc">
+                        <div class="pic">
+                            <a href="#"><img src="http:////cdn.v2ex.com/avatar/f331/db13/25284_normal.png?m=1374281332" alt=""></a>
+                        </div>
+                        <div class="content cc">
+                            <div class="part1">
+                                <a href="#">沪指涨4%逼近2900点 </a>
+                            </div>
+                            <div class="part2">
+                                网易财经12月4日讯 午后，两桶油持续发力，中石油涨停，两市再度上扬，沪指大涨逾4%，盘中最高触及2891.19点，再创43个月以来新高，深成指站上1万点，创22个月新高。截至发稿时，沪指涨4.01%，报2892.69点，成交4297...
+                            </div>
+                            <div class="part3">
+                                <a class="digg-up">顶 100</a>
+                                <a class="digg-down">踩 100</a>
+                                <a class="reply">评论 123</a>
+                                <a class="like">收藏</a>
+                                <span class="tags">
+                                    <a href="#">曼联</a>
+                                    <a href="#">范加尔</a>
+                                    <a href="#">格泽尔家族</a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item cc">
+                        <div class="pic">
+                            <a href="#"><img src="http://cdn.v2ex.com/avatar/baf2/9ae7/39784_normal.png?m=1400686600" alt=""></a>
+                        </div>
+                        <div class="content cc">
+                            <div class="part1">
+                                <a href="#">斯科特:湖人仍能吸引超级球星习惯输球者得离队 </a>
+                            </div>
+                            <div class="part2">
+                                网易体育12月4日报道：据《洛杉矶每日新闻》报道，湖人进入重建期，拜伦-斯科特执教湖人的首个赛季只能忍受不断输球，但对于湖人快速重建成功，斯科特持乐观态度，他表示自己对湖人两大高层库普切克和吉姆-巴斯充满信心。“我完全相信他们能做到，...
+                            </div>
+                            <div class="part3">
+                                <a class="digg-up">顶 100</a>
+                                <a class="digg-down">踩 100</a>
+                                <a class="reply">评论 123</a>
+                                <a class="like">收藏</a>
+                                <span class="tags">
+                                    <a href="#">曼联</a>
+                                    <a href="#">范加尔</a>
+                                    <a href="#">格泽尔家族</a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item cc">
+                        <div class="pic">
+                            <a href="#"><img src="http://cdn.v2ex.com/avatar/5d3b/be32/62234_normal.png?m=1399600102" alt=""></a>
+                        </div>
+                        <div class="content cc">
+                            <div class="part1">
+                                <a href="#">快船28分痛宰魔术6连胜格里芬21+8保罗19+10+5 </a>
+                            </div>
+                            <div class="part2">
+                                网易体育12月4日报道：洛杉矶快船队（13胜5负）在主场延续强势。格里芬三节得到21分、8个篮板和6次助攻，保罗得到19分和10次助攻，他们率队在前三节就建立起20分以上优势，快船队在主场以114-86大胜奥兰多魔术队（7胜14负）。..
+                            </div>
+                            <div class="part3">
+                                <a class="digg-up">顶 100</a>
+                                <a class="digg-down">踩 100</a>
+                                <a class="reply">评论 123</a>
+                                <a class="like">收藏</a>
+                                <span class="tags">
+                                    <a href="#">曼联</a>
+                                    <a href="#">范加尔</a>
+                                    <a href="#">格泽尔家族</a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- center left box end -->
+                <!-- page start -->
+                <div class="pages cc">
+                    <a href="#">上一页</a>
+                    <a href="#">1</a>
+                    <a class="current" href="#">2</a>
+                    <a href="#">3</a>
+                    <a href="#">4</a>
+                    <a href="#">5</a>
+                    <a href="#">6</a>
+                    <a href="#">7</a>
+                    <a href="#">8</a>
+                    <a href="#">下一页</a>
+                    </ul>
+                </div>
+                <!-- page end -->
             </div>
-            <div class="main_right">
+            <!-- center left end -->
+            <!-- center right start -->
+            <div class="main-right">
+                <div class="box">
+                    <a href="#">
+                        <img src="http://www.daqianduan.com/wp-content/uploads/2014/11/ad-qiuji.jpg">
+                    </a>
+                </div>
             </div>
+            <!-- center right end -->
         </div>
     </div>
+    <!-- center end -->
 </div>
 <!--
 <script src="/js/site.js" type="text/javascript" >
